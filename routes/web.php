a9b0aff3b31d4f9018b1b8e74199d23dcc381003
nico <carinan02@gmail.com>
2025-06-17 21:27:51 +0800

    tulog na ako

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ Route::get('/home', function () {
     return view('home');
 });
 Route::get('/nico', function () {
-    return '<h2>Helsana lasssst na</h2>';
+    return '<h2>tulog na ako</h2>';
 });
 Route::get('/about', function () {
     return view('about');

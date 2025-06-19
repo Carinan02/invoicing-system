67a448380bb64e38e7257b7a8acaf83e17e05e77
nico <carinan02@gmail.com>
2025-06-19 21:03:01 +0800

    deploy

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ Route::get('/home', function () {
     return view('home');
 });
 Route::get('/nico', function () {
-    return '<h2>git test</h2>';
+    return '<h2>edited deploy.php</h2>';
 });
 Route::get('/about', function () {
     return view('about');

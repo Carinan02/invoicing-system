651fc479f6c87f5f618cc92ac1c6874fb880c740
nico <carinan02@gmail.com>
2025-06-19 20:35:32 +0800

    di ako mag pull2

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ Route::get('/home', function () {
     return view('home');
 });
 Route::get('/nico', function () {
-    return '<h2>di ako mag pull</h2>';
+    return '<h2>di ako mag pull 2</h2>';
 });
 Route::get('/about', function () {
     return view('about');

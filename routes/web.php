66574ef217e860346ce61fc4fbd04afc570e72d2
nico <carinan02@gmail.com>
2025-06-19 22:29:02 +0800

    new errorss

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ Route::get('/home', function () {
     return view('home');
 });
 Route::get('/nico', function () {
-    return '<h2>new error</h2>';
+    return '<h2>new errorss</h2>';
 });
 Route::get('/about', function () {
     return view('about');

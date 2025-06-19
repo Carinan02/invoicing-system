ebe3f8ab7fe07a79748a2f573826734eb662efd7
nico <carinan02@gmail.com>
2025-06-19 22:20:09 +0800

    added antok

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ Route::get('/home', function () {
     return view('home');
 });
 Route::get('/nico', function () {
-    return '<h2>sana gumana na</h2>';
+    return '<h2>inaantok na ko</h2>';
 });
 Route::get('/about', function () {
     return view('about');

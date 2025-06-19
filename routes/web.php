d9843ab60515adbb16f0609a53a3d34f152eb293
nico <carinan02@gmail.com>
2025-06-19 20:22:52 +0800

    test test

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ Route::get('/home', function () {
     return view('home');
 });
 Route::get('/nico', function () {
-    return '<h2>im back</h2>';
+    return '<h2>test test </h2>';
 });
 Route::get('/about', function () {
     return view('about');

3bbf13f6818290ee1b32351d5ac1d47bc2e03bcb
nico <carinan02@gmail.com>
2025-06-19 21:15:24 +0800

    user who2

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ Route::get('/home', function () {
     return view('home');
 });
 Route::get('/nico', function () {
-    return '<h2>user who</h2>';
+    return '<h2>user who2</h2>';
 });
 Route::get('/about', function () {
     return view('about');

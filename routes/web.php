b4a02c2cd785aa7150d0762acb7234bafeb43e2e
nico <carinan02@gmail.com>
2025-06-19 20:32:05 +0800

    magbabago ba prod

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ Route::get('/home', function () {
     return view('home');
 });
 Route::get('/nico', function () {
-    return '<h2>parang mali</h2>';
+    return '<h2>testing kung magbago ung prod</h2>';
 });
 Route::get('/about', function () {
     return view('about');
